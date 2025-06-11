ed4699af073fdea10440f0e0c98ba7cdba96c144
HP <HP@ROHMAD>
2025-06-11 23:03:06 +0700

    tambah fitur info kelas dan profil

---
+++ JAPRI/View/V_Profil.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace PBO_KelD08.JAPRI.View
+{
+    partial class V_Profil
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(V_Profil));
+            button4 = new Button();
+            button3 = new Button();
+            button2 = new Button();
+            jadwal = new Button();
+            infokelas = new Button();
+            button1 = new Button();
+            nama = new Label();
+            label2 = new Label();
+            setingprofil = new Button();
+            gantipassword = new Button();
+            Logout = new Button();
+            nim = new Label();
+            prodi = new Label();
+            kelaspraktikum = new Label();
+            password = new Label();
+            SuspendLayout();
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.Transparent;
+            button4.FlatAppearance.BorderSize = 0;
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.Location = new Point(88, 367);
+            button4.Name = "button4";
+            button4.Size = new Size(79, 23);
+            button4.TabIndex = 11;
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.Transparent;
+            button3.FlatAppearance.BorderSize = 0;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Location = new Point(88, 323);
+            button3.Name = "button3";
+            button3.Size = new Size(128, 23);
+            button3.TabIndex = 10;
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Transparent;
+            button2.FlatAppearance.BorderSize = 0;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Location = new Point(89, 280);
+            button2.Name = "button2";
+            button2.Size = new Size(127, 23);
+            button2.TabIndex = 9;
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // jadwal
+            // 
+            jadwal.BackColor = Color.Transparent;
+            jadwal.FlatAppearance.BorderSize = 0;
+            jadwal.FlatStyle = FlatStyle.Flat;
+            jadwal.Location = new Point(89, 236);
+            jadwal.Name = "jadwal";
+            jadwal.Size = new Size(78, 23);
+            jadwal.TabIndex = 8;
+            jadwal.UseVisualStyleBackColor = false;
+            // 
+            // infokelas
+            // 
+            infokelas.BackColor = Color.Transparent;
+            infokelas.FlatAppearance.BorderSize = 0;
+            infokelas.FlatStyle = FlatStyle.Flat;
+            infokelas.Location = new Point(88, 195);
+            infokelas.Name = "infokelas";
+            infokelas.Size = new Size(101, 28);
+            infokelas.TabIndex = 7;
+            infokelas.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Transparent;
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Location = new Point(88, 153);
+            button1.Name = "button1";
+            button1.Size = new Size(146, 26);
+            button1.TabIndex = 6;
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // nama
+            // 
+            nama.AutoSize = true;
+            nama.BackColor = Color.Transparent;
+            nama.Font = new Font("Segoe UI", 30.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            nama.ForeColor = SystemColors.GrayText;
+            nama.Location = new Point(354, 40);
+            nama.Name = "nama";
+            nama.Size = new Size(138, 55);
+            nama.TabIndex = 12;
+            nama.Text = "Nama";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.Font = new Font("Segoe UI", 6.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(330, 373);
+            label2.Name = "label2";
+            label2.Size = new Size(0, 12);
+            label2.TabIndex = 14;
+            // 
+            // setingprofil
+            // 
+            setingprofil.BackColor = Color.Transparent;
+            setingprofil.FlatAppearance.BorderSize = 0;
+            setingprofil.FlatStyle = FlatStyle.Flat;
+            setingprofil.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            setingprofil.ForeColor = Color.White;
+            setingprofil.Location = new Point(269, 344);
+            setingprofil.Name = "setingprofil";
+            setingprofil.Size = new Size(154, 23);
+            setingprofil.TabIndex = 15;
+            setingprofil.Text = "Setting Profil";
+            setingprofil.UseVisualStyleBackColor = false;
+            // 
+            // gantipassword
+            // 
+            gantipassword.BackColor = Color.Transparent;
+            gantipassword.FlatAppearance.BorderSize = 0;
+            gantipassword.FlatStyle = FlatStyle.Flat;
+            gantipassword.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            gantipassword.ForeColor = Color.White;
+            gantipassword.Location = new Point(269, 381);
+            gantipassword.Name = "gantipassword";
+            gantipassword.Size = new Size(154, 23);
+            gantipassword.TabIndex = 16;
+            gantipassword.Text = "Ganti Password";
+            gantipassword.UseVisualStyleBackColor = false;
+            // 
+            // Logout
+            // 
+            Logout.BackColor = Color.Transparent;
+            Logout.FlatAppearance.BorderSize = 0;
+            Logout.FlatStyle = FlatStyle.Flat;
+            Logout.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            Logout.ForeColor = Color.White;
+            Logout.Location = new Point(269, 418);
+            Logout.Name = "Logout";
+            Logout.Size = new Size(154, 23);
+            Logout.TabIndex = 17;
+            Logout.Text = "Log Out";
+            Logout.UseVisualStyleBackColor = false;
+            // 
+            // nim
+            // 
+            nim.AutoSize = true;
+            nim.BackColor = Color.Transparent;
+            nim.FlatStyle = FlatStyle.Flat;
+            nim.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            nim.ForeColor = Color.White;
+            nim.Location = new Point(599, 280);
+            nim.Name = "nim";
+            nim.Size = new Size(143, 15);
+            nim.TabIndex = 18;
+            nim.Text = "Nomor Induk mahasiswa";
+            // 
+            // prodi
+            // 
+            prodi.AutoSize = true;
+            prodi.BackColor = Color.Transparent;
+            prodi.FlatStyle = FlatStyle.Flat;
+            prodi.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            prodi.ForeColor = Color.White;
+            prodi.Location = new Point(599, 339);
+            prodi.Name = "prodi";
+            prodi.Size = new Size(87, 15);
+            prodi.TabIndex = 19;
+            prodi.Text = "Program Studi";
+            // 
+            // kelaspraktikum
+            // 
+            kelaspraktikum.AutoSize = true;
+            kelaspraktikum.BackColor = Color.Transparent;
+            kelaspraktikum.FlatStyle = FlatStyle.Flat;
+            kelaspraktikum.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            kelaspraktikum.ForeColor = Color.White;
+            kelaspraktikum.Location = new Point(599, 400);
+            kelaspraktikum.Name = "kelaspraktikum";
+            kelaspraktikum.Size = new Size(97, 15);
+            kelaspraktikum.TabIndex = 20;
+            kelaspraktikum.Text = "Kelas Praktikum";
+            // 
+            // password
+            // 
+            password.AutoSize = true;
+            password.BackColor = Color.Transparent;
+            password.FlatStyle = FlatStyle.Flat;
+            password.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            password.ForeColor = Color.White;
+            password.Location = new Point(599, 454);
+            password.Name = "password";
+            password.Size = new Size(59, 15);
+            password.TabIndex = 21;
+            password.Text = "Password";
+            // 
+            // V_Profil
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            ClientSize = new Size(960, 540);
+            Controls.Add(password);
+            Controls.Add(kelaspraktikum);
+            Controls.Add(prodi);
+            Controls.Add(nim);
+            Controls.Add(Logout);
+            Controls.Add(gantipassword);
+            Controls.Add(setingprofil);
+            Controls.Add(label2);
+            Controls.Add(nama);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(jadwal);
+            Controls.Add(infokelas);
+            Controls.Add(button1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "V_Profil";
+            Text = "Profil";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button4;
+        private Button button3;
+        private Button button2;
+        private Button jadwal;
+        private Button infokelas;
+        private Button button1;
+        private Label nama;
+        private Label label2;
+        private Button setingprofil;
+        private Button gantipassword;
+        private Button Logout;
+        private Label nim;
+        private Label prodi;
+        private Label kelaspraktikum;
+        private Label password;
+    }
+}
---
+++ JAPRI/View/V_Profil.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PBO_KelD08.JAPRI.View
+{
+    public partial class V_Profil : Form
+    {
+        public V_Profil()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void V_Profil_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void V_Profil_Load_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
