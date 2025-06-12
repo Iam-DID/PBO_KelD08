70cbf2ccf95fbed79f103283692897aaa5d02f16
HP <HP@ROHMAD>
2025-06-12 14:19:21 +0700

    tambah fitur ruang kelas dengan nama

---
+++ JAPRI/View/V_Ruang_Kelas_Page.Designer.cs
@@ -0,0 +1,359 @@
+﻿namespace PBO_KelD08.JAPRI.View
+{
+    partial class V_Ruang_Kelas_Page
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            ruang = new Label();
+            lokasigedung = new Label();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            label12 = new Label();
+            label13 = new Label();
+            label14 = new Label();
+            label15 = new Label();
+            label16 = new Label();
+            label17 = new Label();
+            label18 = new Label();
+            SuspendLayout();
+            // 
+            // ruang
+            // 
+            ruang.AutoSize = true;
+            ruang.BackColor = Color.Transparent;
+            ruang.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            ruang.ForeColor = Color.White;
+            ruang.Location = new Point(339, 82);
+            ruang.Name = "ruang";
+            ruang.Size = new Size(70, 25);
+            ruang.TabIndex = 0;
+            ruang.Text = "Ruang";
+            // 
+            // lokasigedung
+            // 
+            lokasigedung.AutoSize = true;
+            lokasigedung.BackColor = Color.Transparent;
+            lokasigedung.FlatStyle = FlatStyle.Flat;
+            lokasigedung.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lokasigedung.ForeColor = Color.White;
+            lokasigedung.Location = new Point(341, 110);
+            lokasigedung.Name = "lokasigedung";
+            lokasigedung.Size = new Size(112, 20);
+            lokasigedung.TabIndex = 1;
+            lokasigedung.Text = "Lokasi Gedung";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.FlatStyle = FlatStyle.Flat;
+            label1.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(341, 198);
+            label1.Name = "label1";
+            label1.Size = new Size(112, 20);
+            label1.TabIndex = 3;
+            label1.Text = "Lokasi Gedung";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(339, 170);
+            label2.Name = "label2";
+            label2.Size = new Size(70, 25);
+            label2.TabIndex = 2;
+            label2.Text = "Ruang";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.Transparent;
+            label3.FlatStyle = FlatStyle.Flat;
+            label3.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.White;
+            label3.Location = new Point(341, 286);
+            label3.Name = "label3";
+            label3.Size = new Size(112, 20);
+            label3.TabIndex = 5;
+            label3.Text = "Lokasi Gedung";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = Color.Transparent;
+            label4.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.ForeColor = Color.White;
+            label4.Location = new Point(339, 258);
+            label4.Name = "label4";
+            label4.Size = new Size(70, 25);
+            label4.TabIndex = 4;
+            label4.Text = "Ruang";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BackColor = Color.Transparent;
+            label5.FlatStyle = FlatStyle.Flat;
+            label5.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.ForeColor = Color.White;
+            label5.Location = new Point(341, 374);
+            label5.Name = "label5";
+            label5.Size = new Size(112, 20);
+            label5.TabIndex = 7;
+            label5.Text = "Lokasi Gedung";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.BackColor = Color.Transparent;
+            label6.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.ForeColor = Color.White;
+            label6.Location = new Point(339, 346);
+            label6.Name = "label6";
+            label6.Size = new Size(70, 25);
+            label6.TabIndex = 6;
+            label6.Text = "Ruang";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.BackColor = Color.Transparent;
+            label7.FlatStyle = FlatStyle.Flat;
+            label7.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label7.ForeColor = Color.White;
+            label7.Location = new Point(341, 460);
+            label7.Name = "label7";
+            label7.Size = new Size(112, 20);
+            label7.TabIndex = 9;
+            label7.Text = "Lokasi Gedung";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.BackColor = Color.Transparent;
+            label8.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label8.ForeColor = Color.White;
+            label8.Location = new Point(339, 432);
+            label8.Name = "label8";
+            label8.Size = new Size(70, 25);
+            label8.TabIndex = 8;
+            label8.Text = "Ruang";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.BackColor = Color.Transparent;
+            label9.FlatStyle = FlatStyle.Flat;
+            label9.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label9.ForeColor = Color.White;
+            label9.Location = new Point(676, 460);
+            label9.Name = "label9";
+            label9.Size = new Size(112, 20);
+            label9.TabIndex = 19;
+            label9.Text = "Lokasi Gedung";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.BackColor = Color.Transparent;
+            label10.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label10.ForeColor = Color.White;
+            label10.Location = new Point(674, 432);
+            label10.Name = "label10";
+            label10.Size = new Size(70, 25);
+            label10.TabIndex = 18;
+            label10.Text = "Ruang";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.BackColor = Color.Transparent;
+            label11.FlatStyle = FlatStyle.Flat;
+            label11.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label11.ForeColor = Color.White;
+            label11.Location = new Point(676, 374);
+            label11.Name = "label11";
+            label11.Size = new Size(112, 20);
+            label11.TabIndex = 17;
+            label11.Text = "Lokasi Gedung";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.BackColor = Color.Transparent;
+            label12.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label12.ForeColor = Color.White;
+            label12.Location = new Point(674, 346);
+            label12.Name = "label12";
+            label12.Size = new Size(70, 25);
+            label12.TabIndex = 16;
+            label12.Text = "Ruang";
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.BackColor = Color.Transparent;
+            label13.FlatStyle = FlatStyle.Flat;
+            label13.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label13.ForeColor = Color.White;
+            label13.Location = new Point(676, 286);
+            label13.Name = "label13";
+            label13.Size = new Size(112, 20);
+            label13.TabIndex = 15;
+            label13.Text = "Lokasi Gedung";
+            // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.BackColor = Color.Transparent;
+            label14.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label14.ForeColor = Color.White;
+            label14.Location = new Point(674, 258);
+            label14.Name = "label14";
+            label14.Size = new Size(70, 25);
+            label14.TabIndex = 14;
+            label14.Text = "Ruang";
+            // 
+            // label15
+            // 
+            label15.AutoSize = true;
+            label15.BackColor = Color.Transparent;
+            label15.FlatStyle = FlatStyle.Flat;
+            label15.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label15.ForeColor = Color.White;
+            label15.Location = new Point(676, 198);
+            label15.Name = "label15";
+            label15.Size = new Size(112, 20);
+            label15.TabIndex = 13;
+            label15.Text = "Lokasi Gedung";
+            // 
+            // label16
+            // 
+            label16.AutoSize = true;
+            label16.BackColor = Color.Transparent;
+            label16.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label16.ForeColor = Color.White;
+            label16.Location = new Point(674, 170);
+            label16.Name = "label16";
+            label16.Size = new Size(70, 25);
+            label16.TabIndex = 12;
+            label16.Text = "Ruang";
+            // 
+            // label17
+            // 
+            label17.AutoSize = true;
+            label17.BackColor = Color.Transparent;
+            label17.FlatStyle = FlatStyle.Flat;
+            label17.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label17.ForeColor = Color.White;
+            label17.Location = new Point(676, 110);
+            label17.Name = "label17";
+            label17.Size = new Size(112, 20);
+            label17.TabIndex = 11;
+            label17.Text = "Lokasi Gedung";
+            // 
+            // label18
+            // 
+            label18.AutoSize = true;
+            label18.BackColor = Color.Transparent;
+            label18.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label18.ForeColor = Color.White;
+            label18.Location = new Point(674, 82);
+            label18.Name = "label18";
+            label18.Size = new Size(70, 25);
+            label18.TabIndex = 10;
+            label18.Text = "Ruang";
+            // 
+            // V_Ruang_Kelas_Page
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = Properties.Resources.Tampilan_ruang_kelas;
+            ClientSize = new Size(960, 540);
+            Controls.Add(label9);
+            Controls.Add(label10);
+            Controls.Add(label11);
+            Controls.Add(label12);
+            Controls.Add(label13);
+            Controls.Add(label14);
+            Controls.Add(label15);
+            Controls.Add(label16);
+            Controls.Add(label17);
+            Controls.Add(label18);
+            Controls.Add(label7);
+            Controls.Add(label8);
+            Controls.Add(label5);
+            Controls.Add(label6);
+            Controls.Add(label3);
+            Controls.Add(label4);
+            Controls.Add(label1);
+            Controls.Add(label2);
+            Controls.Add(lokasigedung);
+            Controls.Add(ruang);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "V_Ruang_Kelas_Page";
+            Text = "V_Info_Kelas_Page";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label ruang;
+        private Label lokasigedung;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private Label label11;
+        private Label label12;
+        private Label label13;
+        private Label label14;
+        private Label label15;
+        private Label label16;
+        private Label label17;
+        private Label label18;
+    }
+}
--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -169,5 +169,15 @@ namespace PBO_KelD08.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Tampilan_ruang_kelas {
+            get {
+                object obj = ResourceManager.GetObject("Tampilan ruang kelas", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
